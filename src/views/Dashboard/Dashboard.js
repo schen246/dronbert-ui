87ed6e19ff46eb50534b48c6e16b00055c56396b
Zhanzhi <hnzjzz@gmail.com>
2020-08-01 10:06:05 -0500

    change period to 5s

--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -123,7 +123,7 @@ const Dashboard = () => {
     //   setCurrentTime(new Date());
     // }, 2 * 1000);
     setCurrentTime(new Date());
-    }, 30 * 1000);
+    }, 5 * 1000);
     return () => clearInterval(timer);
   }, [])
 
